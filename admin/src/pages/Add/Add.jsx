1f023ed54cae9aae96b3f8897738fc70fec617a4
ire0393 <ainairemide@gmail.comm>
2024-05-29 12:31:48 +0100

    modified:   admin/src/App.jsx 	modified:   admin/src/pages/Add/Add.jsx 	new file:   backend/uploads/1716976530493vegetable.png 	new file:   backend/uploads/1716980840215basket.png

--- admin/src/pages/Add/Add.jsx
+++ admin/src/pages/Add/Add.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import './Add.css'
 import { assets } from '../../assets/assets'
 import axios from "axios"
+import { toast } from 'react-toastify'
 
 const Add = () => {
   
@@ -37,6 +38,7 @@ const Add = () => {
             category:"Salad"
           })
           setImage(false)
+          toast.success(response.data.message)
       }
       else {
 
