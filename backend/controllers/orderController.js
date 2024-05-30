984a12147bf21b03eabe89550ae23be9cc588781
ire0393 <ainairemide@gmail.comm>
2024-05-30 17:18:17 +0100

    new commit

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -22,7 +22,7 @@ const placeOrder = async (req,res) =>{
       
       const line_items = req.body.items.map((item)=>({
           price_data:{
-             currency:"dollar",
+             currency:"usd",
              product_data:{
                 name:item.name
              },
@@ -34,7 +34,7 @@ const placeOrder = async (req,res) =>{
 
        line_items.push({
           price_data:{
-             currency:"inr",
+             currency:"usd",
              product_data:{
                 name:"Delivery Charges"
              },
