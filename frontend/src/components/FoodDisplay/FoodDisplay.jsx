e51a446d312740106aca75afaa6e37e3f7308a5f
ire0393 <ainairemide@gmail.comm>
2024-05-31 17:29:10 +0100

    m

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -10,7 +10,7 @@ const FoodDisplay = ({category}) => {
 
   return (
     <div className='food-display' id='food-display'>
-      <h2>Top Dishes near You</h2>
+      <h2>Our Delicious Top Dishes</h2>
        <div className="food-display-list">
          {food_list.map((item,index)=>{
           {console.log(category,item.category);}
